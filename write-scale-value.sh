6f908c5860469c3b1af13cfbec64f35c59f8fe00
Kristofer Sommestad <kristofer.sommestad@springworks.se>
2015-10-28 18:58:17 +0100

    Run write script infinitely

--- write-scale-value.sh
+++ write-scale-value.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 
-TARGET_FILE="/home/pi/scale/scale-result.txt"
+TARGET_FILE=${USBSCALE_RUNNER_TARGET_FILE}
+FILE_OWNER="pi"
 
 # Create file as correct user
-su pi -c touch ${TARGET_FILE}
+sudo -u ${FILE_OWNER} touch ${TARGET_FILE}
 
-# Read scale result and write change to file
-/home/pi/scale/usbscale/usbscale > /home/pi/scale/scale-result.txt
+while true
+do
+	echo "Read value of USB scale..."
+
+	# Read scale result and write change to file
+	RESULT="$(/home/pi/scale/usbscale/usbscale)"
+	echo ${RESULT} > ${TARGET_FILE}
+	echo "Current weight: ${RESULT}"
+done
